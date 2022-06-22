27cba56ce5f47dabc091e6d78f250df9a54b301a
chethu <chethu@chethan>
2022-06-22 13:17:26 +0530

    changes commited

--- com.crm_SDET35/src/test/java/Assignments/CreateContactVerifyTest.java
+++ com.crm_SDET35/src/test/java/Assignments/CreateContactVerifyTest.java
@@ -16,7 +16,7 @@ public class CreateContactVerifyTest extends BaseClass {
 	@Test(retryAnalyzer = com.crm.comcast.genericutility.RetryImpAnalyser.class)
 	public  void createContactVerify() throws Throwable {
 	
-		//to get random number for Chethan Kumar Nairy
+		//to get random number for Chethan Kumar Nairy ckn
 		int randnum = jLib.getrandomnumber();
 		String FirstName = eLib.ReadDataFromExcelFile("Sheet1", 7, 2)+randnum;
 		String LastName = eLib.ReadDataFromExcelFile("Sheet1", 7, 3)+randnum;
